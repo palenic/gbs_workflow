83e9a900e3c5be05567f3e1031b558b5f04800f1
palenic <chiara.paleni.97@gmail.com>
2025-04-01 16:40:00 +0200

    changed stacks inst command, changed denovo

--- scripts/04_denovo_map.sh
+++ scripts/04_denovo_map.sh
@@ -6,5 +6,5 @@ populations=$2;
 
 mkdir -p 04_denovo; # creates the 04_denovo folder if not already created
 
-denovo_map.pl --samples "$mypath" --popmap ./"$populations" --out-path 04_denovo/ --paired -m 3 -M 2 -n 1 -X "ustacks: --force-diff-len" -X "populations: --vcf" --threads 20 --min-samples-per-pop 0.80;
+denovo_map.pl --samples "$mypath" --popmap ./"$populations" --out-path 04_denovo/ --paired -X "ustacks: --force-diff-len" -X "populations: --vcf" --threads 4;
 
